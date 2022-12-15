2b8a53423e20c4b514f9fab86c6149a60ac1a49a
Christian Jiménez <christianjjc@gmail.com>
2022-12-15 13:20:14 -0500

    agregando funciones

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
 
-// algoritmos para preguntar nombre y edad.
-
-let esNombre = false;
-let nombre = prompt(`¡Hola! ¿cuál es tu nombre?`);
-//nombre == null || nombre.trim() == "" || nombre == undefined
-while (esNombre == false) {
-    if (nombre == null || nombre.trim() == "" || nombre == undefined) {
-        nombre = prompt(`Por favor debe ingresar su nombre...`);
-    } else {
-
-        if (isNaN(nombre)) {
-            esNombre=true;
+const ingresaNombre = () => {
+    let esNombre = false;
+    let nombre = prompt(`¡Hola! ¿cuál es tu nombre?`);
+    while (esNombre == false) {
+        if (nombre == null || nombre.trim() == "" || nombre == undefined) {
+            nombre = prompt(`Por favor debe ingresar su nombre...`);
         } else {
-            nombre = prompt(`Por favor, su nombre no puede ser un número...`);
-        };
-    }
+            if (isNaN(nombre)) {
+                esNombre = true;
+                return nombre
+            } else {
+                nombre = prompt(`Por favor, su nombre no puede ser un número...`);
+            };
+        }
+    };
+}
 
-};
-// líneas para desarrollo y pruebas
-console.log(nombre);
-console.log(typeof (nombre));
+const ingresaEdad = (nombre) => {
+    let edad = prompt(`¿Qué edad tienes?`);
+    while (isNaN(edad) || edad == null || edad.trim() == "" || edad == undefined) {
+        edad = prompt(`Por favor, sólo se aceptan números...`);
+    };
+    if (mayorDeEdad(edad)) {
+        alert(`Sr(a) ${nombre.toUpperCase()} . Con ${edad} años, Ud. si tiene permitido el ingreso.`)
+    } else {
+        alert(`Sr(a) ${nombre.toUpperCase()} . Ud. es menor de edad, solo tiene ${edad} años por lo tanto no puede ingresar.`);
+    };
+}
 
-let edad = prompt(`¿Qué edad tienes?`);
-while (isNaN(edad) || edad == null || edad.trim() == "" || edad == undefined) {
-    edad = prompt(`Por favor, sólo se aceptan números...`);
-};
-// líneas para desarrollo y pruebas
-console.log(edad);
-console.log(typeof (edad));
+const mayorDeEdad = (edad) => {
+    if (parseInt(edad) < 18) {
+        return false;
+    } else {
+        return true;
+    };
+}
 
-if (parseInt(edad) < 18) {
-    alert(`Sr(a) ${nombre.toUpperCase()} . Ud. es menor de edad, solo tiene ${edad} años por lo tanto no puede ingresar.`);
-} else {
-    alert(`Sr(a) ${nombre.toUpperCase()} . Con ${edad} años, Ud. si tiene permitido el ingreso.`)
-};
+ingresaEdad(ingresaNombre());
 
